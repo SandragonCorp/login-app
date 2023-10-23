62b2e3515f5324a755c2d2ed874a2cb52e7f172c
SandragonCorp <147520984+SandragonCorp@users.noreply.github.com>
2023-10-24 00:28:11 +0800

    Added register api
    Co-authored-by: Sandro Mendoza <sandro.mendoza@ffufm.com>

--- app/(pages)/user/page.tsx
+++ app/(pages)/user/page.tsx
@@ -3,24 +3,34 @@ import { User } from "@/app/components/_user"
 import { prisma } from "@/app/db";
 import Link from "next/link";
 
-// function getUsers() {
-// return prisma.user.findMany()
-// }
+function getUsers() {
+    return prisma.user.findMany()
+}
+
+async function deleteAllUser() {
+    "use server"
+    await prisma.user.deleteMany({})
+}
 
 export default async function Page() {
-    // const users = await getUsers()
+    const users = await getUsers()
 
     return (
         <>
             <header className="flex justify-between items-center mb-4 p-12">
-                <h1 className="text-2xl">Users</h1>
+                <h1 className="text-2xl bg-black">Users</h1>
                     <Link href="/user/register" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">New</Link>
             </header>
-            <ul className="pl-4">
-                {/* {users.map(user => (
+            <ul className="pl-4 top-8">
+                {users.map(user => (
                     <User key={user.id} {...user} />
-                ))} */}
+                ))}
             </ul>
+            <div>
+            <form action={deleteAllUser} className={`page_wrapper rounded-sm mx-auto text-center`}>
+                    <input type='submit' className='mx-auto mb-16 bg-blue-500 hover:bg-blue-400 text-white font-bold' value='Delete All' />
+                </form>
+            </div>
         </>
     )
 }
