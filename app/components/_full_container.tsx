82fb137d9acf7bf9b6d7e26412c5cde3207c2528
leanfucio <leanviktorfucio@gmail.com>
2023-11-29 00:32:54 +0800

    Fixes #27 Fix fullcontainer

--- app/components/_full_container.tsx
+++ app/components/_full_container.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 interface FullContainerProps {
-  bgImageSrc?: string | null
   id?: string
   className?: string
+  style?: object
   onClick?: () => void
   children?: React.ReactNode
 }
@@ -11,8 +11,8 @@ interface FullContainerProps {
 export const FullContainer = (config: FullContainerProps) => {
   // pt-[72px] is the height of the navbar 
   return (
-    <section id={config.id} className={`w-screen min-h-screen pt-[72px] bg-fixed bg-center bg-no-repeat bg-cover ${config.className}`} style={ config.bgImageSrc ? {backgroundImage: `url(${config.bgImageSrc})`} : {} } onClick={ config.onClick }>
-        { config.children }
+    <section id={config.id} className={`w-screen min-h-screen bg-fixed bg-center bg-no-repeat bg-cover ${config.className}`} style={ config.style } onClick={ config.onClick }>
+      { config.children }
     </section>
   )
 }
