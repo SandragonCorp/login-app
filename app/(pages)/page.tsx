66e2600593e253093fc8db669b93f5120d7d219f
leanfucio <leanviktorfucio@gmail.com>
2023-12-19 23:51:54 +0800

    Fixes #26 additional fixes

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import { FullContainer } from "../components/_full_container";
-import { CAROUSEL_EFFECTS, Carousel } from "../components/_carousel";
+import { CAROUSEL_DURATION, CAROUSEL_EFFECTS, Carousel } from "../components/_carousel";
 import { CarouselItem } from "../components/_carousel_item";
 
 
@@ -13,25 +13,25 @@ export default function Home() {
 	<>
 		<FullContainer>
 			<div className="w-full h-screen">
-				<Carousel effect={CAROUSEL_EFFECTS.FADE} animationDuration={500}>
+				<Carousel effect={CAROUSEL_EFFECTS.SLIDE} animationDuration={CAROUSEL_DURATION.HALF_SECOND} isLoop={true}>
 					<CarouselItem>
 						<div className="w-full h-full pt-32 text-center text-white bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(/images/home-carousel/1.jpg)`}}>
-							<div className="text-9xl"><span className="bg-yellow-400/[.3]">Attack On Titan</span></div>
+							<div className="text-9xl"><span className="bg-yellow-400/[.3]">1 Attack On Titan</span></div>
 						</div>
 					</CarouselItem>
 					<CarouselItem>
 						<div className="w-full h-full pt-32 text-center text-white bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(/images/home-carousel/2.jpg)`}}>
-							<div className="text-9xl"><span className="bg-yellow-400/[.3]">Steins;Gate</span></div>
-						</div>
-					</CarouselItem>
-					<CarouselItem className="active">
-						<div className="w-full h-full pt-32 text-center text-white bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(/images/home-carousel/3.png)`}}>
-							<div className="text-9xl"><span className="bg-yellow-400/[.3]">Fate/Stay Night: Unlimited Blade Works</span></div>
+							<div className="text-9xl"><span className="bg-yellow-400/[.3]">2 Steins;Gate</span></div>
 						</div>
 					</CarouselItem>
 					<CarouselItem>
+						<div className="w-full h-full pt-32 text-center text-white bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(/images/home-carousel/3.png)`}}>
+							<div className="text-9xl"><span className="bg-yellow-400/[.3]">3 Fate/Stay Night: Unlimited Blade Works</span></div>
+						</div>
+					</CarouselItem>
+					<CarouselItem className="active">
 						<div className="w-full h-full pt-32 text-center text-white bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(/images/home-carousel/4.jpg)`}}>
-							<div className="text-9xl"><span className="bg-yellow-400/[.3]">Full Metal Alchemist: Brotherhood</span></div>
+							<div className="text-9xl"><span className="bg-yellow-400/[.3]">4 Full Metal Alchemist: Brotherhood</span></div>
 						</div>
 					</CarouselItem>
 				</Carousel>
--- app/components/_carousel.tsx
+++ app/components/_carousel.tsx
@@ -11,15 +11,22 @@ export enum CAROUSEL_EFFECTS {
     FADE = "fade"
 }
 
-// const CAROUSEL_TRANSITION_CLASSES = {
-//     [CAROUSEL_EFFECTS.SLIDE] : "transition-left",
-//     [CAROUSEL_EFFECTS.FADE] : "transition-opacity",
-// }
+export enum CAROUSEL_DIRECTION {
+    LEFT = "LEFT",
+    RIGHT = "RIGHT"
+}
+
+export enum CAROUSEL_DURATION {
+    HALF_SECOND = 500,
+    ONE_SECOND = 1000,
+    TWO_SECONDS = 2000
+}
 
 interface CarouselProps {
     className?: string
     effect: CAROUSEL_EFFECTS
-    animationDuration: number
+    animationDuration: CAROUSEL_DURATION
+    isLoop: boolean
     children: JSX.Element[]
 }
 
@@ -42,19 +49,17 @@ export const Carousel = (config: CarouselProps) => {
 
     // setup for carousel
 
-    // hide carousel buttons
-    // we don't use 
+    // hide carousel buttons 
     let isHideLeftButton = false;
     let isHideRightButton = false;
-    if (carouselItemActiveIndex === 0) {
-        isHideLeftButton = true;
-    } else if (carouselItemActiveIndex === carouselItemsCount - 1) {
-        isHideRightButton = true;
-    }
 
-    let carouselWindowLeft = 0;
-    if (config.effect === CAROUSEL_EFFECTS.SLIDE) {
-        carouselWindowLeft = carouselItemActiveIndex * -100;
+    // isLoop = true means all buttons are always shown
+    if (!config.isLoop) {
+        if (carouselItemActiveIndex === 0) {
+            isHideLeftButton = true;
+        } else if (carouselItemActiveIndex === carouselItemsCount - 1) {
+            isHideRightButton = true;
+        }
     }
 
     // render carousel items
@@ -69,9 +74,9 @@ export const Carousel = (config: CarouselProps) => {
         };
 
         if (config.effect === CAROUSEL_EFFECTS.SLIDE) {
-            props.style = {
-                left: index * 100 + "%"
-            };
+            // props.style = {
+            //     left: index * 100 + "%"
+            // };
         } else if (config.effect === CAROUSEL_EFFECTS.FADE) {
             props.style = {
                 left: "0%"
@@ -103,8 +108,15 @@ export const Carousel = (config: CarouselProps) => {
         if (isCarouselMoving(carousel)) {return}
 
         let nextCarouselItemActiveIndex = getCarouselItemActiveIndex(carousel);
-        if (direction === "right") nextCarouselItemActiveIndex++;
-        if (direction === "left") nextCarouselItemActiveIndex--;
+        if (direction === CAROUSEL_DIRECTION.RIGHT) nextCarouselItemActiveIndex++;
+        if (direction === CAROUSEL_DIRECTION.LEFT) nextCarouselItemActiveIndex--;
+
+        // nextCarouselItemActiveIndex should always exist
+        if (nextCarouselItemActiveIndex < 0) {
+            nextCarouselItemActiveIndex = carouselItemsCount - 1;
+        } else if (nextCarouselItemActiveIndex >= carouselItemsCount) {
+            nextCarouselItemActiveIndex = 0;
+        }
 
         showNextCarouselItem(e, nextCarouselItemActiveIndex);
     };
@@ -133,9 +145,9 @@ export const Carousel = (config: CarouselProps) => {
             carouselItemNextActive.classList.add("next-active");
 
             if (config.effect === CAROUSEL_EFFECTS.SLIDE) {
-                showNewCarouselItemBySlide(carouselItemActive, carouselItemNextActive, nextCarouselItemActiveIndex);
+                showNewCarouselItemBySlide(carouselItemActive, carouselItemNextActive);
             } else if (config.effect === CAROUSEL_EFFECTS.FADE) {
-                showNewCarouselItemByFade(carouselItemActive, carouselItemNextActive, nextCarouselItemActiveIndex);
+                showNewCarouselItemByFade(carouselItemActive, carouselItemNextActive);
             }
 
             // update carousel indicators
@@ -158,34 +170,46 @@ export const Carousel = (config: CarouselProps) => {
         const oldCarouselItemActive = carousel.getElementsByClassName('carousel-item active')[0];
         const newCarouselItemActive = carousel.getElementsByClassName('carousel-item next-active')[0];
 
+        // reset everything
         oldCarouselItemActive.classList.remove("moving");
         oldCarouselItemActive.classList.remove("active");
         oldCarouselItemActive.classList.remove("!z-10");
         oldCarouselItemActive.classList.remove("!opacity-100");
+        oldCarouselItemActive.classList.remove("transition-property-opacity");
+        oldCarouselItemActive.classList.remove("transition-property-left");
+        oldCarouselItemActive.classList.remove("!-left-full");
+        oldCarouselItemActive.classList.remove("!left-full");
 
         newCarouselItemActive.classList.remove("moving");
         newCarouselItemActive.classList.remove("next-active");
         newCarouselItemActive.classList.remove("!z-10");
         newCarouselItemActive.classList.remove("!opacity-100");
+        newCarouselItemActive.classList.remove("transition-property-opacity");
+        newCarouselItemActive.classList.remove("transition-property-left");
+        newCarouselItemActive.classList.remove("!-left-full");
+        newCarouselItemActive.classList.remove("!left-full");
+
+        // add new active class
         newCarouselItemActive.classList.add("active");
 
         const carouselItemActiveIndex = parseInt(newCarouselItemActive.getAttribute("data-index")!);
         carousel.setAttribute("data-current-active-index", carouselItemActiveIndex.toString());
 
-        toggleButtons(carousel);
+        // toggle buttons only if not isLoop
+        if (!config.isLoop) toggleButtons(carousel);
     }
 
     return (
         <div className={`carousel h-full w-full overflow-hidden ${config.className ?? ''} ${config.effect}`} data-current-active-index={carouselItemActiveIndex}>
-            <div className={`carousel-slide-container h-full w-full transition-left duration-${config.animationDuration}`} style={{left: carouselWindowLeft + "%"}}>
+            <div className={`carousel-slide-container h-full w-full transition-left duration-${config.animationDuration}`} >
                 { React.Children.map(config.children, (child: JSX.Element, index) => preRenderCarouselItem(child, index)) }
             </div>
-            <div className={`left-button absolute w-16 h-16 top-1/2 left-0 translate-y-N50Percent cursor-pointer ${isHideLeftButton ? 'hidden' : ''}`} onClick={(e: SyntheticEvent) => onCarouselArrowClick(e, "left")}>
+            <div className={`left-button absolute w-16 h-16 top-1/2 left-0 translate-y-N50Percent cursor-pointer ${isHideLeftButton ? 'hidden' : ''}`} onClick={(e: SyntheticEvent) => onCarouselArrowClick(e, CAROUSEL_DIRECTION.LEFT)}>
                 <div className='flex justify-center items-center h-full w-full bg-gray-600/[0.4] hover:bg-gray-600/[0.8] rounded-full'>
                     <FontAwesomeIcon icon={faAngleLeft} size="2xl" style={{ color: "#ffffff" }} />
                 </div>
             </div>
-            <div className={`right-button absolute w-16 h-16 top-1/2 right-0 translate-y-N50Percent cursor-pointer ${isHideRightButton ? 'hidden' : ''}`} onClick={(e: SyntheticEvent) => onCarouselArrowClick(e, "right")}>
+            <div className={`right-button absolute w-16 h-16 top-1/2 right-0 translate-y-N50Percent cursor-pointer ${isHideRightButton ? 'hidden' : ''}`} onClick={(e: SyntheticEvent) => onCarouselArrowClick(e, CAROUSEL_DIRECTION.RIGHT)}>
                 <div className='flex justify-center items-center h-full w-full bg-gray-600/[0.4] hover:bg-gray-600/[0.8] rounded-full'>
                     <FontAwesomeIcon icon={faAngleRight} size="2xl" style={{ color: "#ffffff" }} />
                 </div>
@@ -199,19 +223,46 @@ export const Carousel = (config: CarouselProps) => {
     )
 }
 
-const showNewCarouselItemBySlide = (carouselItemActive: Element, carouselItemNextActive: Element, nextCarouselItemActiveIndex: number): void => {
-    const carouselSlidesContainer = carouselItemActive.parentElement; // .carousel-slide-container
+const showNewCarouselItemBySlide = (carouselItemActive: Element, carouselItemNextActive: Element): void => {
+    carouselItemActive.classList.add("moving");
+    carouselItemNextActive.classList.add("moving");
 
-    carouselSlidesContainer!.style.left = nextCarouselItemActiveIndex * -100 + "%";
+    // get direction from data-index's
+    const direction = parseInt(carouselItemActive.getAttribute('data-index')!) > parseInt(carouselItemNextActive.getAttribute('data-index')!) ? CAROUSEL_DIRECTION.LEFT : CAROUSEL_DIRECTION.RIGHT;
+
+    if (direction === CAROUSEL_DIRECTION.RIGHT) {
+        carouselItemNextActive.classList.add("!left-full");
+    } else if (direction === CAROUSEL_DIRECTION.LEFT) {
+        carouselItemNextActive.classList.add("!-left-full");
+    }
+
+    carouselItemActive.classList.add("transition-property-left");
+    carouselItemNextActive.classList.add("transition-property-left");
+
+    // we use timeout because somehow, the transition-property-left is not in the dom yet when we update the left position
+    // so we put the next logic in the other event loop
+    setTimeout(() => {
+        if (direction === CAROUSEL_DIRECTION.RIGHT) {
+            carouselItemActive.classList.add("!-left-full");
+            carouselItemNextActive.classList.remove("!left-full");
+        } else if (direction === CAROUSEL_DIRECTION.LEFT) {
+            carouselItemActive.classList.add("!left-full");
+            carouselItemNextActive.classList.remove("!-left-full");
+        }
+    });
 }
 
-const showNewCarouselItemByFade = (carouselItemActive: Element, carouselItemNextActive: Element, nextCarouselItemActiveIndex: number) => {
+const showNewCarouselItemByFade = (carouselItemActive: Element, carouselItemNextActive: Element) => {
     carouselItemActive.classList.add("moving");
-
     carouselItemNextActive.classList.add("moving");
+
+    carouselItemNextActive.classList.add("!opacity-0");
+
+    carouselItemNextActive.classList.add("transition-property-opacity");
+
     setTimeout(() => {
         carouselItemNextActive.classList.add("!z-10");
-        carouselItemNextActive.classList.add("!opacity-100");
+        carouselItemNextActive.classList.remove("!opacity-100");
     });
 }
 
